5cab7d1da790311277a3ce84bfd41f6ad6690808
ShambhaviKu <158810332+ShambhaviKu@users.noreply.github.com>
2025-03-31 14:45:23 +0530

    first commit

---
+++ input_fun.py
@@ -0,0 +1,23 @@
+name = input("Enter your name: ")
+
+age = input("Enter your age: ")
+
+Roll_no = input("Enter your Roll No: ")
+
+phone_no = input("Enter your phone no: ")
+
+print(name, age, Roll_no, phone_no, end = '\n')
+print(name)
+
+#print table of 2 using print command
+
+print(2*1)
+print(2*2)
+print(2*3)
+print(2*4)
+print(2*5)
+print(2*6)
+print(2*7)
+print(2*8)
+print(2*9)
+print(2*10)
